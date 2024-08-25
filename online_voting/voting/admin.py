e58a6956edde44d6b5084e2532779a1937ef5edf
Yan <yanpaingwin@ucsy.edu.mm>
2024-08-25 09:13:56 +0630

    Update

--- online_voting/voting/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import VotingEvent,Candidate,Vote,Category,Profile
-
-admin.site.register(VotingEvent)
-admin.site.register(Candidate)
-admin.site.register(Vote)
-admin.site.register(Category)
-admin.site.register(Profile)
