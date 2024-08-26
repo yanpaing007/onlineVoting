8208dba24ba1a0d4e2c2ca9a7d3edd955b10c653
Kaung2240 <kaungmyathtun@ucsy.edu.mm>
2024-08-27 01:59:38 +0630

    serverside timezone handling resolved.

--- online_voting/online_voting/settings.py
+++ online_voting/online_voting/settings.py
@@ -63,7 +63,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
