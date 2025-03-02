1a9567fc487fe9fb395510bb37e34b3ea9024cd2
Yan <yanpaingwin@ucsy.edu.mm>
2025-03-02 13:48:15 +0630

    Update Home Page UI

---
+++ online_voting/voting/models/__init__.py
@@ -0,0 +1,6 @@
+from .category import Category
+from .voting_event import VotingEvent
+from .candidate import Candidate
+from .vote import Vote
+from .profile import Profile
+from .favorite import Favorite
